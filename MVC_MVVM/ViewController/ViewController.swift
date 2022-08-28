6d66dd3a31810724dbdc48c75efd8b52a673dc50
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-08-29 01:16:58 +0900

    docs: 주석제거

--- MVC_MVVM/ViewController/ViewController.swift
+++ MVC_MVVM/ViewController/ViewController.swift
@@ -11,8 +11,8 @@ class ViewController: UIViewController {
 
     let tableView = UITableView()
     
-    var sampleModel:SampleModel = SampleModel(name: "first", age: 333)
     var dataManager = DataManager.shared
+    var sampleModel:SampleModel = SampleModel(name: "first", age: 333)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,105 +28,35 @@ class ViewController: UIViewController {
                 print("\(change.oldValue) -> \(change.newValue)")
                 self.tableView.reloadData()
             }
-            self.sampleModel.name = model.name// model 이름이 바뀌었다면 tableview reload.
+            
+            // model 이름이 바뀌었다면 tableview reload.
+            self.sampleModel.name = model.name
             observer.invalidate()
+            
         }
         
         //MARK: TableView
         view.addSubview(tableView)
         tableView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
         
-        
     }
 }
 extension ViewController: fetchDelegate {
     func fetchSampleModel(_ sampleModel: SampleModel) {
-        print("from: delegate", sampleModel.name)
+//        print("from: delegate", sampleModel.name)
         self.tableView.reloadData()
     }
 }
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.cellID, for: indexPath) as? TableViewCell else { return UITableViewCell() }
         cell.nameLabel.text = sampleModel.name
         return cell
-//        return UITableViewCell()
     }
 }
 
-extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor? = nil,
-                left: NSLayoutXAxisAnchor? = nil,
-                bottom: NSLayoutYAxisAnchor? = nil,
-                right: NSLayoutXAxisAnchor? = nil,
-                paddingTop: CGFloat = 0,
-                paddingLeft: CGFloat = 0,
-                paddingBottom: CGFloat = 0,
-                paddingRight: CGFloat = 0,
-                width: CGFloat? = nil,
-                height: CGFloat? = nil) {
 
-        translatesAutoresizingMaskIntoConstraints = false
-
-        if let top = top {
-            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
-        }
-
-        if let left = left {
-            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
-        }
-
-        if let bottom = bottom {
-            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
-        }
-
-        if let right = right {
-            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
-        }
-
-        if let width = width {
-            widthAnchor.constraint(equalToConstant: width).isActive = true
-        }
-
-        if let height = height {
-            heightAnchor.constraint(equalToConstant: height).isActive = true
-        }
-    }
-
-    func centerX(inView view: UIView) {
-        translatesAutoresizingMaskIntoConstraints = false
-        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    }
-
-    func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil,
-                 paddingLeft: CGFloat = 0, constant: CGFloat = 0) {
-
-        translatesAutoresizingMaskIntoConstraints = false
-        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
-
-        if let left = leftAnchor {
-            anchor(left: left, paddingLeft: paddingLeft)
-        }
-    }
-
-    func setDimensions(height: CGFloat, width: CGFloat) {
-        translatesAutoresizingMaskIntoConstraints = false
-        heightAnchor.constraint(equalToConstant: height).isActive = true
-        widthAnchor.constraint(equalToConstant: width).isActive = true
-    }
-
-    func setHeight(height: CGFloat) {
-        translatesAutoresizingMaskIntoConstraints = false
-        heightAnchor.constraint(equalToConstant: height).isActive = true
-    }
-
-    func setWidth(width: CGFloat) {
-        translatesAutoresizingMaskIntoConstraints = false
-        widthAnchor.constraint(equalToConstant: width).isActive = true
-    }
-
-}
