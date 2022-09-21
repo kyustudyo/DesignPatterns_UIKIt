df33d6e3e626d769d83cf936810829d93c9bac0a
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-09-21 10:52:36 +0900

    refactor: 테이블뷰 표현방식

--- MVC_MVVM/Model/Event.swift
+++ MVC_MVVM/Model/Event.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 class Event {
     
@@ -13,3 +14,23 @@ class Event {
     
     //TODO: 가져온 데이터들을 다루는 코드 추가
 }
+
+
+class Event2: Publisher {
+    
+    typealias Output = String
+    typealias Failure = Never
+        
+    func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        
+//        DispatchQueue.global(qos: .utility).async {
+            let dummy: [String] = ["jack", "tom"]
+            dummy.forEach {
+               _ = subscriber.receive($0)
+            }
+//            subscriber.receive(completion: .finished)
+//        }
+    }
+    
+}
+
--- MVC_MVVM/ViewController/ViewController.swift
+++ MVC_MVVM/ViewController/ViewController.swift
@@ -6,11 +6,24 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
-    let orderTableView = UITableView()
-    let PhoneNumberTableView = UITableView()
+    private lazy var orderTableView: UITableView = {
+        $0.dataSource = self
+        $0.delegate = self
+        $0.register(OrderTableViewCell.self, forCellReuseIdentifier: OrderTableViewCell.cellID)
+        return $0
+    }(UITableView())
+    
+    private lazy var PhoneNumberTableView: UITableView = {
+        $0.dataSource = self
+        $0.delegate = self
+        $0.register(PhoneNumberTableViewCell.self, forCellReuseIdentifier: PhoneNumberTableViewCell.cellID)
+        return $0
+    }(UITableView())
+    
     
     var dataManager = DataManager.shared
     
@@ -34,18 +47,11 @@ class ViewController: UIViewController {
         }
     }
     
+    var eventPublisher = Event2()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray6
-        orderTableView.delegate = self
-        orderTableView.dataSource = self
-        
-        PhoneNumberTableView.delegate = self
-        PhoneNumberTableView.dataSource = self
-        
-        orderTableView.register(OrderTableViewCell.self, forCellReuseIdentifier: OrderTableViewCell.cellID)
-        PhoneNumberTableView.register(PhoneNumberTableViewCell.self, forCellReuseIdentifier: PhoneNumberTableViewCell.cellID)
-//        dataManager.delegate = self // 질문
         
         // 1. completion handler로 모델에서 Controller로.
         // 2. 여기서는 KVO가 궂이 필요없다. property의 변화를 보고싶다면 필요하다.
@@ -57,6 +63,11 @@ class ViewController: UIViewController {
         orderTableView.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 0, paddingRight: 0, height: 200)
         PhoneNumberTableView.anchor(top: orderTableView.bottomAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 100, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
         
+    }
+    private func bind() {
+        _ = eventPublisher.sink(receiveValue: {
+            print($0)
+        })
     }
     //MARK: User Interact -> Model Update -> View Update
     private func fetchData() {
