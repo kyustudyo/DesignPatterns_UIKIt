e23a9f2cacd38cdbca62d5a767986903a3e8325f
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-09-29 23:12:43 +0900

    touch interaction 추가

--- Repository/ViewController/MenuViewController.swift
+++ Repository/ViewController/MenuViewController.swift
@@ -21,9 +21,19 @@ class MenuViewController: UIViewController {
         return $0
     }(UITableView())
     
+    private let tappedCellIndexLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 14, weight: .bold)
+        $0.backgroundColor = .white
+        $0.text = "셀을 클릭하세요"
+        return $0
+    }(UILabel())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        view.backgroundColor = .white
+        
+        
         viewModel.MenusSubject
 //            .catchAndReturn([])
 //            .observe(on: MainScheduler.instance)
@@ -32,19 +42,30 @@ class MenuViewController: UIViewController {
                 cell.nameLabel.text = item.name
                 cell.priceLabel.text = "\(item.price)"
             }
-        
+            .disposed(by: disposeBag)
 //            .bind(to: tableView.rx.items(cellIdentifier: MenuTableViewCell.cellId, cellType: MenuTableViewCell.self)) { index, item, cell in
 //                cell.nameLabel.text = item.name
 //                cell.priceLabel.text = "\(item.price)"
 //            }
         
+        tableView.rx.itemSelected
+            .map{ "\($0)"}
+            .asDriver(onErrorJustReturn: "")
+            .drive(tappedCellIndexLabel.rx.text)
             .disposed(by: disposeBag)
+            
     }
     
     private func setUI() {
-        view.addSubview(tableView)
+        
+        view.addSubviews(tappedCellIndexLabel, tableView)
+        tappedCellIndexLabel.snp.makeConstraints { make in
+            make.top.leading.equalTo(view.safeAreaLayoutGuide).offset(8)
+        }
+        
         tableView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.bottom.leading.trailing.equalToSuperview()
+            make.top.equalTo(tappedCellIndexLabel.snp.bottom)
         }
     }
     
